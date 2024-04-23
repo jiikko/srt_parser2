29d3de97656b727ca4bf710b704e6522a84cfcf8
jiikko <n905i.1214@gmail.com>
2024-04-23 19:55:19 +0900

    時間は浮動小数で返す

--- lib/srt_parser2/line.rb
+++ lib/srt_parser2/line.rb
@@ -5,19 +5,20 @@ module SrtParser2
     attr_writer :start_time, :end_time
 
     def filled?
-      sequence.is_a?(Integer) && start_time.is_a?(Integer) && end_time.is_a?(Integer) && error.nil?
+      sequence.is_a?(Integer) && start_time.is_a?(Numeric) && end_time.is_a?(Numeric) && error.nil?
     end
 
     def start_time
-      to_i(@start_time)
+      to_f(@start_time)
     end
 
     def end_time
-      to_i(@end_time)
+      to_f(@end_time)
     end
 
     def next_sequence
       raise 'invalid impliment!!!!!!!' if sequence.nil?
+
       sequence + 1
     end
 
@@ -27,19 +28,16 @@ module SrtParser2
 
     private
 
-    def to_i(time_str)
+    def to_f(time_str)
       return nil if time_str.nil?
 
       hours, minutes, seconds_and_millis = time_str.split(':')
       seconds, millis = seconds_and_millis.split(',')
 
-      total_seconds =
-        hours.to_i * 3600 +
+      hours.to_i * 3600 +
         minutes.to_i * 60 +
         seconds.to_i +
         (millis.to_f / 1000)
-
-      total_seconds.to_i
     end
   end
 end
